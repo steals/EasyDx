b293db23a545a8f9d9f641bb41e19f41a059194a
Eugene Rusalev <eugene.rusalev@jetbi.com>
2019-09-15 17:29:43 +0300

    Restructed code folders, updated package versions

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 module.exports = {
+  mode: "development",
   entry: ["./src/js/app.js"],
   output: {
     path: path.resolve(__dirname, "dist"),
@@ -12,7 +14,10 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
         }
       },
       {
@@ -29,6 +34,13 @@ module.exports = {
     new HtmlWebPackPlugin({
       template: "./src/index.html",
       filename: "./index.html"
-    })
+    }),
+    new CopyWebpackPlugin([
+      { from: "src/css", to: "css" },
+      { from: "src/img", to: "img" },
+      { from: "src/fonts", to: "fonts" },
+      { from: "src/index.fonts.css", to: "" },
+      { from: "src/index.styles.css", to: "" }
+    ])
   ]
 };
