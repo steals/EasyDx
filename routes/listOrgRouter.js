84e2430792b20c9e69877aa248a421476840a173
Eugene Rusalev <eugene.rusalev@jetbi.com>
2019-09-17 16:57:57 +0300

    Allow refresh of org list without default project

--- routes/listOrgRouter.js
+++ routes/listOrgRouter.js
@@ -11,50 +11,34 @@ listOrgRouter.use(bodyParser.json());
 
 listOrgRouter.route('/')
 .post((req, res) => {
-    let directory = req.body.directory;
-    const sfdxProjFileName = 'sfdx-project.json';
-    const isWin = process.platform === "win32";
-    let directoryDelimeter = "/";
-    if(isWin) {
-        directoryDelimeter = "\\";
-    }
-
-    fs.access(directory + directoryDelimeter + sfdxProjFileName, (err) => {
-        if(err) {
-            res.statusCode = 202;
-            res.send({err: 'The default project directory doesnot exist or doesnot contain a valid sfdx project!'});
-            console.log(err);
-            return;
-        }
-        cmd.get(
-            `cd ${directory} && sfdx force:org:list --json`,
-            function(err, data, stderr) {
-                if(!err) {
-                    let dataObj = JSON.parse(data);
-                    for(let i = 0; i < dataObj.result.nonScratchOrgs.length; i++) {
-                        dataObj.result.nonScratchOrgs[i].accessToken = "";
-                    }
-                    for(let i = 0; i < dataObj.result.scratchOrgs.length; i++) {
-                        dataObj.result.scratchOrgs[i].accessToken = "";
-                    }
-                    res.statusCode = 200;
-                    res.send(JSON.stringify(dataObj));
-
-                    const orgs = dataObj.result;
-                    const jsonObj = {};
-                    jsonObj.orgs = orgs;
-
-                    jsonfile.writeFile(orgFile, jsonObj, function(error) {
-
-                    });
-                } else {
-                    res.statusCode = 202;
-                    let errObj = JSON.parse(stderr);
-                    res.send({"err": errObj.message});
+    cmd.get(
+        'sfdx force:org:list --json',
+        function(err, data, stderr) {
+            if(!err) {
+                let dataObj = JSON.parse(data);
+                for(let i = 0; i < dataObj.result.nonScratchOrgs.length; i++) {
+                    dataObj.result.nonScratchOrgs[i].accessToken = "";
                 }
+                for(let i = 0; i < dataObj.result.scratchOrgs.length; i++) {
+                    dataObj.result.scratchOrgs[i].accessToken = "";
+                }
+                res.statusCode = 200;
+                res.send(JSON.stringify(dataObj));
+
+                const orgs = dataObj.result;
+                const jsonObj = {};
+                jsonObj.orgs = orgs;
+
+                jsonfile.writeFile(orgFile, jsonObj, function(error) {
+
+                });
+            } else {
+                res.statusCode = 202;
+                let errObj = JSON.parse(stderr);
+                res.send({"err": errObj.message});
             }
-        );
-    });
+        }
+    );
 });
 
 module.exports = listOrgRouter;
