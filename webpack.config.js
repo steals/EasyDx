3a72935437113602e27a3b15ade7f04353853369
Eugene Rusalev <eugene.rusalev@jetbi.com>
2019-09-22 16:05:36 +0300

    prettify webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
-const path = require("path");
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+
 module.exports = {
-  mode: "development",
-  entry: ["./src/js/app.js"],
+  mode: 'development',
+  entry: ['./src/js/app.js'],
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "js/[name].js"
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].js',
   },
   module: {
     rules: [
@@ -14,36 +15,36 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"]
-          }
-        }
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
       },
       {
         test: /\.html$/,
         use: [
           {
-            loader: "html-loader"
-          }
-        ]
-      }
-    ]
+            loader: 'html-loader',
+          },
+        ],
+      },
+    ],
   },
   watchOptions: {
-    ignored: /node_modules/
+    ignored: /node_modules/,
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html"
+      template: './src/index.html',
+      filename: './index.html',
     }),
     new CopyWebpackPlugin([
-      { from: "src/css", to: "css" },
-      { from: "src/img", to: "img" },
-      { from: "src/fonts", to: "fonts" },
-      { from: "src/index.fonts.css", to: "" },
-      { from: "src/index.styles.css", to: "" }
-    ])
-  ]
+      { from: 'src/css', to: 'css' },
+      { from: 'src/img', to: 'img' },
+      { from: 'src/fonts', to: 'fonts' },
+      { from: 'src/index.fonts.css', to: '' },
+      { from: 'src/index.styles.css', to: '' },
+    ]),
+  ],
 };
