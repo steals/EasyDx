e91e4a2047565878c6d8d2bbe5c948b2ff9e9883
Eugene Rusalev <eugene.rusalev@jetbi.com>
2019-10-27 22:06:32 +0300

    Allow CORS

--- src/server/index.js
+++ src/server/index.js
@@ -35,6 +35,12 @@ const app = express();
 const port = process.env.PORT || 3666;
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.use('/api/org', orgRouter);
 app.use('/api/listOrg', listOrgRouter);
 app.use('/api/defaultOrg', defaultOrgRouter);
